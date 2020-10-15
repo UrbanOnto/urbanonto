acc249de993c613ce83c824704af55f1271f7f0a
Pawel Garbacz <pawel.garbacz@makolab.com>
2020-10-15 15:07:16 +0100

    New reportinh mechanism
    Signed-off-by: Pawel Garbacz <pawel.garbacz@makolab.com>

--- urbanonto_tools/excel_exporters/exporter.py
+++ urbanonto_tools/excel_exporters/exporter.py
@@ -17,11 +17,12 @@ def export_all_entities_from_excel_file_to_ontology(excel_file_path: str, ontolo
     excel_sheet_dataframes = pandas.read_excel(excel_file_path, sheet_name=None, header=None)
 
     object_count = 0
-    for excel_sheet_dataframe in excel_sheet_dataframes.values():
+    for excel_sheet_name, excel_sheet_dataframe in excel_sheet_dataframes.items():
         object_count += 1
         for index, row in excel_sheet_dataframe.iterrows():
             ontology = \
                 __export_row(
+                    excel_sheet_name=excel_sheet_name,
                     index=index,
                     row=row,
                     ontology=ontology,
@@ -40,7 +41,7 @@ def __split_cell_into_literals(cell: str, subject, predicate, lang: str, ontolog
         ontology.add((subject,predicate,object))
 
 
-def __export_row(index, row, ontology: Graph, ontology_with_imports: Graph, object_count: int):
+def __export_row(excel_sheet_name: str, index, row, ontology: Graph, ontology_with_imports: Graph, object_count: int):
     pl_row_value = str(row[1]).replace('nan', '')
     en_row_value = str(row[2]).replace('nan', '')
 
@@ -54,30 +55,43 @@ def __export_row(index, row, ontology: Graph, ontology_with_imports: Graph, obje
         if len(pl_row_value) > 0:
             name_pl_literal = Literal(pl_row_value, lang='pl')
             ontology.add((entity, RDFS.label, name_pl_literal))
+        else:
+            print('No PL name in', excel_sheet_name)
         if len(en_row_value) > 0:
             name_en_literal = Literal(en_row_value, lang='en')
             ontology.add((entity, RDFS.label, name_en_literal))
+        else:
+            print('No EN name in', excel_sheet_name)
     if index == DEFINITION_ROW_NO:
         if len(pl_row_value) > 0:
             def_pl_literal = Literal(pl_row_value, lang='pl')
             ontology.add((entity, RDFS.isDefinedBy, def_pl_literal))
+        else:
+            print('No PL definition in', excel_sheet_name)
         if len(en_row_value) > 0:
             def_en_literal = Literal(en_row_value, lang='en')
             ontology.add((entity, RDFS.isDefinedBy, def_en_literal))
+        else:
+            print('No EN definition in', excel_sheet_name)
     if index == FUNCTION_ROW_NO:
         if len(pl_row_value) > 0:
-            __add_functions_to_entity(functions_string=pl_row_value, entity=entity, ontology=ontology, ontology_with_imports=ontology_with_imports)
+            __add_functions_to_entity(functions_string=pl_row_value, entity=entity, ontology=ontology, ontology_with_imports=ontology_with_imports, excel_sheet_name=excel_sheet_name)
+        else:
+            print('No function in', excel_sheet_name)
     if index == COMMENT_ROW_NO:
         if len(pl_row_value) > 0:
             comment_pl_literal = Literal(pl_row_value, lang='pl')
             ontology.add((entity, RDFS.comment, comment_pl_literal))
+        else:
+            print('No PL comment in', excel_sheet_name)
         if len(en_row_value) > 0:
             comment_en_literal = Literal(en_row_value, lang='en')
             ontology.add((entity, RDFS.comment, comment_en_literal))
+        else:
+            print('No EN comment in', excel_sheet_name)
     if index == SYNONYM_PL_ROW_NO:
         if len(pl_row_value) > 0:
-            __split_cell_into_literals(cell=pl_row_value, subject=entity, predicate=SKOS.altLabel, lang='pl',
-                                       ontology=ontology)
+            __split_cell_into_literals(cell=pl_row_value, subject=entity, predicate=SKOS.altLabel, lang='pl', ontology=ontology)
     if index == SYNONYM_EN_ROW_NO:
         if len(pl_row_value) > 0:
             __split_cell_into_literals(cell=pl_row_value, subject=entity, predicate=SKOS.altLabel, lang='en',
@@ -117,13 +131,15 @@ def __export_row(index, row, ontology: Graph, ontology_with_imports: Graph, obje
             bdot_label = Literal(pl_row_value, datatype=XSD.string)
             bdot_objects = list(ontology_with_imports.subjects(predicate=RDFS.label, object=bdot_label))
             if len(bdot_objects) == 0:
-                print('Creating', str(entity), 'I was not able to find BDOT name', bdot_label)
+                print('Exporting from', excel_sheet_name, 'I was not able to find BDOT name', bdot_label, 'mentioned in', excel_sheet_name)
             else:
                 if len(bdot_objects) > 1:
-                    print('Creating', str(entity), 'multiple BDOT10K objects were found for', bdot_label)
+                    print('Exporting from', excel_sheet_name, 'multiple BDOT10K objects were found for', bdot_label, 'in', excel_sheet_name)
                 else:
                     bdot_object = bdot_objects[0]
                     ontology.add((entity, RDFS.subClassOf, bdot_object))
+        else:
+            print('No BDOT10K class in', excel_sheet_name)
     if index == WIKIDATA_ROW_NO:
         if len(pl_row_value) > 0:
             wikidata_id_literal = Literal('https://www.wikidata.org/wiki/' + pl_row_value, datatype=XSD.url)
@@ -134,21 +150,23 @@ def __export_row(index, row, ontology: Graph, ontology_with_imports: Graph, obje
             author = URIRef(pl_row_value)
             ontology.add((author, RDF.type, URIRef('http://www.cidoc-crm.org/cidoc-crm/E21_Person')))
             ontology.add((entity, URIRef('http://purl.org/dc/terms/creator'), author))
+        else:
+            print('No author in', excel_sheet_name)
 
     return ontology
 
 
-def __add_functions_to_entity(functions_string:str, entity: URIRef, ontology: Graph, ontology_with_imports: Graph):
+def __add_functions_to_entity(excel_sheet_name: str, functions_string:str, entity: URIRef, ontology: Graph, ontology_with_imports: Graph):
     functions = functions_string.split(',')
     for function in functions:
         function_stripped = function.strip()
         function_label = Literal(function_stripped, lang='pl')
         function_objects = list(ontology_with_imports.subjects(predicate=RDFS.label, object=function_label))
         if len(function_objects) == 0:
-            print('Creating', str(entity), 'I was not able to find function with name', function)
+            print('Exporting from', excel_sheet_name, 'I was not able to find function with name', function)
         else:
             if len(function_objects) > 1:
-                print('Creating', str(entity), 'multiple functions were found for name', function)
+                print('Exporting from', excel_sheet_name, 'multiple functions were found for name', function)
             else:
                 function_object = function_objects[0]
                 owl_value_restriction_for_function, ontology = \
