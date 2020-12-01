83cb5b30dfbd8044e5780f63aa91c9a794d844d2
Pawel Garbacz <pawel.garbacz@makolab.com>
2020-12-01 18:20:49 +0000

    new function added
    Signed-off-by: Pawel Garbacz <pawel.garbacz@makolab.com>

--- urbanonto_tools/start_ups/importer.py
+++ urbanonto_tools/start_ups/importer.py
@@ -1,3 +1,3 @@
 from orchestrators.excel_import_orchestrator import orchestrate_import
 
-orchestrate_import(excel_file_path=r'../data/new_definitions_202011301517.xlsx', ontology_iri=r'../data/urbanonto.ttl')
+orchestrate_import(excel_file_path=r'../data/new_definitions_202012011914.xlsx', ontology_iri=r'../data/urbanonto.ttl')
