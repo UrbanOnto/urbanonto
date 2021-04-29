379c35374263c973d82be9d33bfccea1cd87f47d
Pawel Garbacz <pawel.garbacz@makolab.com>
2021-04-29 15:29:30 +0100

    new input data format
    Signed-off-by: Pawel Garbacz <pawel.garbacz@makolab.com>

--- urbanonto_tools/database/import_from_excel_to_database.py
+++ urbanonto_tools/database/import_from_excel_to_database.py
@@ -1,15 +1,38 @@
+import argparse
 import logging
 
 from database.export_from_excel.excel_exporter import export_to_csv_files
 from database.import_to_database.import_orchestrator import orchestrate_import
 
+
+# if __name__ == "__main__":
+#     parser = argparse.ArgumentParser(description='Export data from Excel to postgres database')
+#     parser.add_argument('--folder', help='Path to folder', metavar='FOLDER')
+#     parser.add_argument('--file', help='File name', metavar='FILE')
+#     parser.add_argument('--user', help='User name', metavar='USER')
+#     parser.add_argument('--password', help='Password', metavar='PASSWORD')
+#     args = parser.parse_args()
+#
+#     logging.basicConfig(format='%(levelname)s %(asctime)s %(message)s', level=logging.WARNING ,datefmt='%m/%d/%Y %I:%M:%S %p')
+#
+#     export_to_csv_files(
+#         folder_name=args.folder,
+#         excel_file_name=args.file)
+#
+#     orchestrate_import(
+#         csv_files_folder_path=args.folder,
+#         host='localhost',
+#         port='5432',
+#         database='urbanonto',
+#         user=args.user,
+#         password=args.password)
+
 export_to_csv_files(
-    excel_folder_name=r'D:\projects\ihpan\urbanonto\database\inputs\db_csv_files',
-    excel_file_name='urbanonto_inputs_202104261547.xlsx',
-    version='urbanonto_inputs_202104261547')
+    folder_name=r'D:\projects\ihpan\urbanonto\database\inputs\db_csv_files',
+    excel_file_name='urbanonto_inputs_202104261547.xlsx')
 
 orchestrate_import(
-    manifestation_csv_files_folder_path=r'D:\projects\ihpan\urbanonto\database\inputs\db_csv_files',
+    csv_files_folder_path=r'D:\projects\ihpan\urbanonto\database\inputs\db_csv_files',
     host='localhost',
     port='5432',
     database='urbanonto',
--- urbanonto_tools/database/import_to_database/import_orchestrator.py
+++ urbanonto_tools/database/import_to_database/import_orchestrator.py
@@ -4,8 +4,7 @@ from database.import_to_database.db_utils.database_connector import connect_to_d
 from database.import_to_database.importers.csv_files_importer import import_data_from_csv_files
 
 
-def orchestrate_import(manifestation_csv_files_folder_path: str, host: str, port: str, database: str, user: str, password: str):
-    logging.basicConfig(format='%(levelname)s %(asctime)s %(message)s', level=logging.INFO,datefmt='%m/%d/%Y %I:%M:%S %p')
+def orchestrate_import(csv_files_folder_path: str, host: str, port: str, database: str, user: str, password: str):
     db_cursor = connect_to_db(host=host, port=port, database=database,user=user, password=password)
-    import_data_from_csv_files(cursor=db_cursor, manifestation_csv_files_folder_path=manifestation_csv_files_folder_path)
+    import_data_from_csv_files(cursor=db_cursor, manifestation_csv_files_folder_path=csv_files_folder_path)
     db_cursor.close()
