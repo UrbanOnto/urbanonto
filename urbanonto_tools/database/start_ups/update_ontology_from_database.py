e85f55fa610511dd91303ad26b859eaf6ffdea0a
Pawel Garbacz <pawel.garbacz@makolab.com>
2021-05-10 15:15:18 +0100

    new function added
    Signed-off-by: Pawel Garbacz <pawel.garbacz@makolab.com>

--- urbanonto_tools/database/start_ups/update_ontology_from_database.py
+++ urbanonto_tools/database/start_ups/update_ontology_from_database.py
@@ -2,9 +2,9 @@ from database.import_to_database.db_utils.database_connector import connect_to_d
 from database.import_to_database.update_orchestrator import database_update_orchestrator
 
 database_update_orchestrator(
-    host='hgis.ihpan.edu.pl',
-    port='5432',
-    database='house_work',
-    user='garbacz',
-    password='Mereolog13',
-    ontology_iri='http://purl.org/urbanonto')
+    host='***',
+    port='***',
+    database='***',
+    user='***',
+    password='***',
+    ontology_iri='***')
