727781757a21fa3cea6b2a9ccf57b503878c586f
mereolog <pawel.garbacz@makolab.com>
2021-08-26 12:35:40 +0200

    db update confirmation added to urban_tools
    Signed-off-by: mereolog <pawel.garbacz@makolab.com>

--- urbanonto_tools/database_initial_population/import_to_database/reference_dataset_importers/ontology_data_importer.py
+++ urbanonto_tools/database_initial_population/import_to_database/reference_dataset_importers/ontology_data_importer.py
@@ -2,6 +2,7 @@ import logging
 
 from rdflib import Graph, URIRef
 
+from common.guis.db_update_confirmation_gui import DbGuiConfirmations
 from database_initial_population.import_to_database.reference_dataset_importers.sparql_queries.sparql_queries import \
     get_types_of_topographic_objects, get_functions_of_topographic_objects
 from database_initial_population.import_to_database.sparql_importer import import_data_from_sparql_query_to_db_table
@@ -9,6 +10,10 @@ from ontology_initial_import.owl_handlers.owl_importer import add_recursively_ow
 
 
 def import_data_from_ontology(ontology_iri: str, cursor):
+    db_update_confirmation_gui = DbGuiConfirmations()
+    if not db_update_confirmation_gui.update:
+        return
+
     logging.info(msg='Loading ' + ontology_iri)
     ontology = Graph()
     ontology.parse(ontology_iri, format='n3')
@@ -21,7 +26,7 @@ def import_data_from_ontology(ontology_iri: str, cursor):
         sparql_query=get_types_of_topographic_objects,
         ontology=ontology,
         cursor=cursor,
-        table_name='test.topographic_types',
+        table_name='ontology_sources.topographic_types',
         columns=['iri', 'name'],
         resolve_conflict=True)
 
@@ -29,6 +34,6 @@ def import_data_from_ontology(ontology_iri: str, cursor):
         sparql_query=get_functions_of_topographic_objects,
         ontology=ontology,
         cursor=cursor,
-        table_name='test.functions',
+        table_name='ontology_sources.functions',
         columns=['iri', 'name'],
         resolve_conflict=True)
--- urbanonto_tools/database_ontology_synchronisation/database_synchroniser.py
+++ urbanonto_tools/database_ontology_synchronisation/database_synchroniser.py
@@ -1,6 +1,5 @@
 from common.db_utils.database_connector import connect_to_db
 from common.guis.db_connection_gui import DbGuiConnections
-from common.guis.db_update_confirmation_gui import DbGuiConfirmations
 from database_initial_population.import_to_database.reference_dataset_importers.ontology_data_importer import \
     import_data_from_ontology
 
@@ -8,9 +7,8 @@ from database_initial_population.import_to_database.reference_dataset_importers.
 def synchronise_ontology_with_database():
     db_connection_gui = DbGuiConnections()
     cursor = connect_to_db(host=db_connection_gui.host,port=db_connection_gui.port,database=db_connection_gui.database,user=db_connection_gui.user,password=db_connection_gui.password)
-    db_update_confirmation_gui = DbGuiConfirmations()
-    if db_update_confirmation_gui.update:
-        import_data_from_ontology(cursor=cursor, ontology_iri='http://purl.org/urbanonto')
+    import_data_from_ontology(cursor=cursor, ontology_iri='http://purl.org/urbanonto')
+
 
 
 if __name__ == '__main__':
